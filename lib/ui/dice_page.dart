7f77330685d9cd185f833183597d621d74a44a25
ErAbhi90 <er.abhithakral@gmail.com>
2019-05-19 00:54:43 +0530

    init commit

---
+++ lib/ui/dice_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+/*This is the main Screen of the app */
+class DicePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: Colors.red,
+      ),
+      child: SafeArea(
+        child: Container(),
+      ),
+    );
+  }
+}
